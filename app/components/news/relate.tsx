a329a509039f1d27830dc8a27a3719383d2f220d
Sasipinya <mysasipinya@gmail.com>
2024-12-03 17:29:02 +0700

    update

--- app/components/news/relate.tsx
+++ app/components/news/relate.tsx
@@ -23,7 +23,7 @@ function RelateNews({ data_relate }: { data_relate?: any }) {
                         <div className="md:my-4 p-3 flex flex-col justify-between">
                             <Link href={`/program/${item.program_slug}`} className='none text-xs md:block 2xl:text-sm md:text-xs  text-gray-600'>{item.program_name}</Link>
 
-                            <h3 ><Link href={`/news/${item.news_id}`} className='text-base md:text-lg  text-gray-600 hover:text-red-700 line-clamp-2' >{item.news_title}</Link></h3>
+                            <h3 ><Link href={`/news/${item.news_id}`} className='text-base font-medium md:text-lg  text-gray-600 hover:text-red-700 line-clamp-2' >{item.news_title}</Link></h3>
                             <div>
                                 <div className=" md:py-4 md:pb-2 flex">
                                     <div className='px-1 py-1 mr-1 mb-2 flex align-items-center'>
