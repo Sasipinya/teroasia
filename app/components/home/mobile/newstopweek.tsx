38385404f01f48a9b03417441577cb2718e26638
Sasipinya <mysasipinya@gmail.com>
2024-12-03 17:18:46 +0700

    update

--- app/components/home/mobile/newstopweek.tsx
+++ app/components/home/mobile/newstopweek.tsx
@@ -1,10 +1,8 @@
 
-'use client';
-
 import type { FC } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { NumberFormat } from '../../utils/kformat';
 
 
 
@@ -107,7 +105,7 @@ const NewsCard: FC<{ news: NewsItem; imageHeight: number; imageWidth: number }> 
                             d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
                         />
                     </svg>
-                    {news.news_count} ครั้ง
+                    {NumberFormat(news.news_count)} 
                 </div>
             </div>
         </div>
