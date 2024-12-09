84638885f51aff12b6eff5f8f5d02dda75b8d604
Sasipinya <mysasipinya@gmail.com>
2024-12-09 14:43:36 +0700

    update

--- app/components/news/info.tsx
+++ app/components/news/info.tsx
@@ -7,7 +7,7 @@ function InfoNews({ data }: { data?: any }) {
 
         <>
             
-            <div className='text-gray-700 text-lg  flex align-item-center'> <Link href={`/`}>หน้าหลัก</Link> <ChevronRight className='w-8 h-8' /><OptimizedImage className={` border  rounded-full w-50 h-50 mr-2 shadow-lg`} src={data.program_mini_icon} width={50} height={50}  alt={data.program_name} priority={false} /><Link href={`/program/${data.program_slug}`} className='flex '>{data.program_name}</Link> </div>
+            <div className='text-gray-700 text-lg  flex items-center '> <Link href={`/`}>หน้าหลัก</Link> <ChevronRight className='w-8 h-8' /><OptimizedImage className={` border  rounded-full w-50 h-50 mr-2 shadow-lg`} src={data.program_mini_icon} width={50} height={50}  alt={data.program_name} priority={false} /><Link href={`/program/${data.program_slug}`} className='flex '>{data.program_name}</Link> </div>
             <h1 className='text-gray-700 text-3xl font-bold mb-2 flex'>{data.news_title}</h1>
             <div className='text-gray-700 text-lg  flex align-items-center mb-3'>
                 <Eye className="w-6 h-6 text-gray-700 mr-1" />{NumberFormat(data.news_count)}
