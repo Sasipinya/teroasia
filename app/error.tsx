2f2bc1cc079500f6c406182469d2ba9bc3da0827
Sasipinya <sasipinya@MacBook-Pro-khxng-Sasipinya.local>
2024-12-12 00:56:50 -0800

    update

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Error({ error, reset, }: { error: Error ,reset: () => void }) {
+    return (
+        <div>
+            <h1>error, some thing went wrong!</h1>
+        </div>
+    );
+}
