ae940db17df95427ad231c407d9989d31a741951
Sasipinya <sasipinya@MacBook-Pro-khxng-Sasipinya.local>
2024-12-12 02:22:11 -0800

    update

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         hostname: 'backend.teroasia.com',
       },
     ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp', 'image/avif'],
+    minimumCacheTTL: 60,
   },
   experimental: {
     forceSwcTransforms: true,
