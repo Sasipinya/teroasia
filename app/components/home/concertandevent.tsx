9fbd89fa3984f3dfc8a1eb241fda406a305aaa6a
Sasipinya <sasipinya@MacBook-Pro-khxng-Sasipinya.local>
2024-12-11 22:40:18 -0800

    update

--- app/components/home/concertandevent.tsx
+++ app/components/home/concertandevent.tsx
@@ -26,7 +26,7 @@ function Concertandevent({ data }: ConcertandeventProps) {
     return (
         <div className="hidden md:block container mx-auto my-6 p-2">
             <div className='flex'>
-                <h3 className="mb-6 text-2xl font-bold  text-gray-600 mr-4"><span className="text-red-700">TERO</span> MUSIC</h3><div className='flex-1 my-4 border-t border-gray-600'></div>
+                <h3 className="mb-6 text-2xl font-bold  text-gray-600 mr-4"><span className="text-red-700">TERO</span> CONCERTS & EVENTS</h3><div className='flex-1 my-4 border-t border-gray-600'></div>
             </div>
 
             <Swiper
