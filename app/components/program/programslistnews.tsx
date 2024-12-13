589cca0834917cd0b9182b16b484670f24e2c93f
Sasipinya <mysasipinya@gmail.com>
2024-12-13 11:00:30 +0700

    update

--- app/components/program/programslistnews.tsx
+++ app/components/program/programslistnews.tsx
@@ -41,7 +41,7 @@ function ProgramsListNews({ data }: { data: any }) {
         <div className="w-full  md:w-[320px] p-2 pb-2 md:pb-0">
           <div
             style={{ backgroundColor: data.info.tvp_color }}
-            className={`h-full rounded flex flex-col justify-between`}
+            className={`h-full rounded-md flex flex-col justify-between`}
           >
             <div className=" p-3 mt-3 w-full flex justify-center">
               <OptimizedImage
