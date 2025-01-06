702153f3e4f417cddc86a3d42aeba2c4c3f2b79d
Sasipinya <mysasipinya@gmail.com>
2025-01-06 13:29:39 +0700

    update

--- app/components/home/programslistnews.tsx
+++ app/components/home/programslistnews.tsx
@@ -7,53 +7,37 @@ import { Calendar, Eye } from 'lucide-react'
 
 
 
-interface ProgramsListNewsProps {
-  data: Array<{
-    info: {
-      tvp_color: string;
-      tvp_logo: string;
-      program_name: string;
-      program_slug: string;
-      program_desc: string;
-    };
-    items: Array<{
-      news_id: number;
-      news_title: string;
-      image_url: string;
-      news_count: number;
-      news_strdate: string;
-    }>;
-  }>;
-}
-
-const ProgramsListNews = ({ data }: ProgramsListNewsProps) => {
+const ProgramsListNews = ({ data }: { data: any }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div >
       {data.map((program: any, index: number) => (
-        <div key={index}>
-          <div className="flex flex-col justify-between h-full">
-            <div className="grid grid-cols-3 gap-1">
+         <div className="container mx-auto mb-10" key={index}>
+        <div  className="flex flex-row justify-between md:flex-row-reverse">
+          <div className="w-full md:w-2/3">
+            <div className="flex flex-wrap">
               {program.items &&
                 program.items.length > 0 &&
                 program.items.map((item: any, index: number) => (
-                  <div key={"item" + index} className="flex flex-col">
-                    <Link href={`/news/${item.news_id}`}>
-                      <OptimizedImage
-                        src={item.image_url}
-                        alt={item.news_title}
-                        width={300}
-                        height={100}
-                        className="rounded-tl-md rounded-tr-md"
-                      />
-                    </Link>
-                    <div className="px-3 py-4">
-                      <div className="mb-2">
-                        <Link
-                          href={`/news/${item.news_id}`}
-                          className=" text-base text-gray-700 line-clamp-2 hover:text-red-700  cursor-pointer"
-                        >
-                          {item.news_title}
-                        </Link>
+                  <div className="w-1/3 p-1" key={"item" + index}>
+                    <div className="max-w-sm rounded overflow-hidden border shadow-md">
+                      <Link href={`/news/${item.news_id}`}>
+                        <OptimizedImage
+                          src={item.image_url}
+                          alt={item.news_title}
+                          width={300}
+                          height={100}
+                          className="w-full rounded-tl-md rounded-tr-md"
+                        />
+                      </Link>
+                      <div className="px-3 py-4">
+                        <div className="mb-2">
+                          <Link
+                            href={`/news/${item.news_id}`}
+                            className=" text-base text-gray-700 line-clamp-2 hover:text-red-700  cursor-pointer"
+                          >
+                            {item.news_title}
+                          </Link>
+                        </div>
                       </div>
                       <div className="flex p-2 border-t justify-between">
                         <div className="mr-3 flex align-items-center">
@@ -73,7 +57,12 @@ const ProgramsListNews = ({ data }: ProgramsListNewsProps) => {
                   </div>
                 ))}
             </div>
-            <div className="flex flex-col justify-between bg-white rounded-md shadow-md">
+          </div>
+          <div className="w-full md:w-1/3 p-2">
+            <div
+              style={{ backgroundColor: program.info.tvp_color }}
+              className="h-full rounded-md flex flex-col justify-between"
+            >
               <div className="p-3 mt-3 w-full flex justify-center">
                 <OptimizedImage
                   src={program.info.tvp_logo}
@@ -84,10 +73,10 @@ const ProgramsListNews = ({ data }: ProgramsListNewsProps) => {
                 />
               </div>
               <div className="text-center px-3 mb-5">
-                <h1 className=" text-xl font-bold text-gray-700">
+                <h1 className=" text-xl font-bold text-white">
                   {program.info.program_name}
                 </h1>
-                <h2 className=" text-sm text-gray-700 mb-4">
+                <h2 className=" text-sm text-white mb-4">
                   {program.info.program_desc}
                 </h2>
                 <div className="w-full flex justify-center">
@@ -100,6 +89,7 @@ const ProgramsListNews = ({ data }: ProgramsListNewsProps) => {
               </div>
             </div>
           </div>
+        </div>
         </div>
       ))}
     </div>
