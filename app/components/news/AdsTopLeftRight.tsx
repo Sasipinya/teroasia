d366c7378b1d3596faa2ef9f8dac68c6950d458a
Sasipinya <mysasipinya@gmail.com>
2025-01-07 13:40:30 +0700

    update

--- app/components/news/AdsTopLeftRight.tsx
+++ app/components/news/AdsTopLeftRight.tsx
@@ -82,12 +82,7 @@ const AdsTopLeftRight: React.FC<SponsorProps> = ({ base,id_ads,path_ads }) => {
             
                 <div
                     className={`z-[-1] min-w-[300px] min-h-[600px] absolute  ${base}`} 
-                    style={{
-                        top: scrollY > sponsorAHeight ? (scrollY < bodyHeight ? '69px' : `${diff}px`) : '69px',
-                        position: scrollY > sponsorAHeight ? 'fixed' : 'absolute',
-                        
-                    }}
-                    ref={divElement}
+                    
                 >
                      {/* AdsNews */}
                      
