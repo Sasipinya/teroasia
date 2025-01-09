d7c8510386ac06f56cfe44992bacee7993bdf2ba
Sasipinya <mysasipinya@gmail.com>
2025-01-09 17:41:32 +0700

    update

---
+++ lib/utils/showheroesmobile.tsx
@@ -0,0 +1,35 @@
+
+'use client';
+
+import { useEffect } from 'react';
+
+const ShowHeroesMobile = ({ showheroes_signature }: { showheroes_signature: string }) => {
+
+  useEffect(() => {
+    const adContainerId = 'showheroes_mobile';
+    const adUrl = `https://content.viralize.tv/display/?zid=AAEy2QyWJ-VWLafc&cid=${showheroes_signature}&activation=adfill-onview&vip_mode=playing&vip_position=bottom-right`;
+
+    const script = document.createElement('script');
+    script.src = adUrl;
+    script.async = true;
+    script.setAttribute('data-wid', 'auto');
+
+    const container = document.getElementById(adContainerId);
+    if (container) {
+      container.appendChild(script);
+    } else {
+      console.error('Ad container not found!');
+    }
+
+    return () => {
+      if (container) {
+        container.removeChild(script);
+      }
+    };
+  }, [showheroes_signature]);
+
+  return <div id="showheroes_mobile"></div>;
+
+}
+
+export default ShowHeroesMobile;
