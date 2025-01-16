71676ea9683b2b6ed083a0669e47bb056a6e78c4
Sasipinya <mysasipinya@gmail.com>
2025-01-16 11:41:57 +0700

    update

--- app/components/news/mobile/titlenews.tsx
+++ app/components/news/mobile/titlenews.tsx
@@ -8,8 +8,8 @@ function TitleNews({ data }: TitleNewsProps) {
     return (
 
         <>
-            <div className='container mx-auto p-2'>
-                    <h1 className='text-gray-700 text-2xl font-medium mb-2 flex'>{data?.news_title}</h1>
+            <div className='container mx-auto px-2 py-4'>
+                    <h1 className='text-gray-700 text-3xl font-medium mb-2 flex'>{data?.news_title}</h1>
                 </div> 
 
 
