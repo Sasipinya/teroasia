3c49acec93e17b6ce59cfbba68039a841b912cf4
Sasipinya <mysasipinya@gmail.com>
2025-01-16 10:44:34 +0700

    update

---
+++ app/components/program/mobile/AdsTopMobile.tsx
@@ -0,0 +1,59 @@
+
+import AdUnit from '../../utils/AdUnit';
+
+
+
+interface AdsConfig {
+    id_ads_bg: string;
+    path_ads_bg: string;
+
+}
+
+const AdsTopMobile =() => {
+
+
+
+    const getAdsConfig = (): AdsConfig => {
+        const defaultConfig: AdsConfig = {
+            id_ads_bg: 'div-gpt-ad-1676442792350-0',
+            path_ads_bg: "/33368840/TA_Desktop_Program_Billboard",
+
+        };
+        return defaultConfig;
+    };
+
+    const adsConfig = getAdsConfig();
+
+    return (
+        <>
+            <div
+                className={`cursor-pointer bg-none`}
+            >
+                {adsConfig.path_ads_bg && (
+                    <div className="container mx-auto">
+                        <div className="mx-2">
+
+                            <div
+                                id={adsConfig.id_ads_bg}
+                                className="min-w-[970px] min-h-[250px]"
+                            >
+                                <AdUnit
+                                    id={adsConfig.id_ads_bg}
+                                    adUnitPath={adsConfig.path_ads_bg}
+                                    size={[[970, 250], [1120, 300]]}
+                                    Mxauto='mx-auto'
+                                />
+                            </div>
+
+                        </div>
+                    </div>
+                )}
+
+            </div>
+
+
+        </>
+    );
+};
+
+export default AdsTopMobile;
--- app/program/[slug]/page.tsx
+++ app/program/[slug]/page.tsx
@@ -12,6 +12,8 @@ import ProgramsListNews from '@/app/components/program/programslistnews';
 import AdsTop from '@/app/components/program/AdsTop';
 import AdsLeftRight from '@/app/components/program/AdsLeftRight';
 import AdUnit from '@/app/components/utils/AdUnit';
+import MobileOnly from '@/app/components/utils/MobileCheck';
+import AdsTopMobile from '@/app/components/program/mobile/AdsTopMobile';
 
 async function fetchProgramItem(slug: string) {
     // Your data fetching logic
@@ -55,52 +57,95 @@ export default async function Page({
 
     return (
         <>
-
+            {/* Desktop */}
             <main className="flex flex-col">
                 <AdsTop />
                 <div className='flex container mx-auto '>
                     {/* Ads Left News */}
                     <AdsLeftRight id_ads='div-gpt-ad-1676442847735-0' path_ads='/33368840/TA_Desktop_Program_SideSkyscraper_Left' base='left-0' />
                     <div>
-                    <div className='container mx-auto px-4 md:p-0 bg-white'>
-                        <div className='flex align-items-center'>
-                            <div><OptimizedImage className={` border  rounded-xl w-[80px] h-[80px] md:w-[120px] md:h-[120px]    mr-2 shadow-lg`} src={data.info.program_mini_icon} width={150} height={150} alt={data.info.program_name} /></div>
-                            <div className='flex flex-col my-auto '>
-                                <div className='flex'>
-                                    <div className='text-gray-700 text-base md:text-lg  flex align-item-center'> <Link href={`/`}>หน้าหลัก</Link> <ChevronRight className='w-5 h-5 md:w-8 md:h-8' /><Link href={`/program/${data.info.program_slug}`} className='flex font-bold '>{data.info.program_name}</Link> </div>
-                                </div>
-                                <div className='flex'>
-                                    <ShareProgram data={data} />
+                        <div className='container mx-auto px-4 md:p-0 bg-white'>
+                            <div className='flex align-items-center'>
+                                <div><OptimizedImage className={` border  rounded-xl w-[80px] h-[80px] md:w-[120px] md:h-[120px]    mr-2 shadow-lg`} src={data.info.program_mini_icon} width={150} height={150} alt={data.info.program_name} /></div>
+                                <div className='flex flex-col my-auto '>
+                                    <div className='flex'>
+                                        <div className='text-gray-700 text-base md:text-lg  flex align-item-center'> <Link href={`/`}>หน้าหลัก</Link> <ChevronRight className='w-5 h-5 md:w-8 md:h-8' /><Link href={`/program/${data.info.program_slug}`} className='flex font-bold '>{data.info.program_name}</Link> </div>
+                                    </div>
+                                    <div className='flex'>
+                                        <ShareProgram data={data} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='container mx-auto bg-white'>
-                        <OptimizedImage className={`    w-full h-full  shadow-lg`} src={data.info.tvp_img_main} width={700} height={550} alt={data.info.program_name} />
-                    </div>
-                    <div className='container mx-auto bg-white text-center my-10'>
-                        <h1 className='text-gray-700 font-bold text-3xl'>{data.info.program_name}</h1>
-                        <h2 className='text-gray-600 text-xl'>{data.info.program_desc}</h2>
-                    </div>
-                    <div className='container mx-auto bg-white'>
-                        <SlideNews data={data.news_feature.items} />
-                    </div>
-                    <div className='container mx-auto bg-white min-w-[300px] min-h-[250px]'>
-                    <AdUnit id='div-gpt-ad-1676443162388-0' adUnitPath='/33368840/TA_Desktop_Program_MidLeaderboard' size={[[728, 90], [970, 250], [970, 90], [1120, 300]]} />
-                    </div>
-                    <div className='container mx-auto bg-white text-gray-700 text-xl p-4 md:p-0 md:text-3xl'>
-                        {parse(data.info.tvp_profile_html)}
-                    </div>
-                    <div className='container mx-auto bg-white'>
-                        <ProgramsListNews data={data} />
-                    </div>
+                        <div className='container mx-auto bg-white'>
+                            <OptimizedImage className={`    w-full h-full  shadow-lg`} src={data.info.tvp_img_main} width={700} height={550} alt={data.info.program_name} />
+                        </div>
+                        <div className='container mx-auto bg-white text-center my-10'>
+                            <h1 className='text-gray-700 font-bold text-3xl'>{data.info.program_name}</h1>
+                            <h2 className='text-gray-600 text-xl'>{data.info.program_desc}</h2>
+                        </div>
+                        <div className='container mx-auto bg-white'>
+                            <SlideNews data={data.news_feature.items} />
+                        </div>
+                        <div className='container mx-auto bg-white min-w-[300px] min-h-[250px]'>
+                            <AdUnit id='div-gpt-ad-1676443162388-0' adUnitPath='/33368840/TA_Desktop_Program_MidLeaderboard' size={[[728, 90], [970, 250], [970, 90], [1120, 300]]} />
+                        </div>
+                        <div className='container mx-auto bg-white text-gray-700 text-xl p-4 md:p-0 md:text-3xl'>
+                            {parse(data.info.tvp_profile_html)}
+                        </div>
+                        <div className='container mx-auto bg-white'>
+                            <ProgramsListNews data={data} />
+                        </div>
                     </div>
                     {/* Ads Right News */}
                     <AdsLeftRight id_ads='div-gpt-ad-1676442884131-0' path_ads='/33368840/TA_Desktop_Program_SideSkyscraper_Right' base='right-0' />
                 </div>
-
-
             </main >
+            {/* Mobile */}
+            <MobileOnly>
+            <main className="flex flex-col md:hidden">
+                <AdsTopMobile />
+                <div className='flex container mx-auto '>
+                    
+                    <div>
+                        <div className='container mx-auto px-4 md:p-0 bg-white'>
+                            <div className='flex align-items-center'>
+                                <div><OptimizedImage className={` border  rounded-xl w-[80px] h-[80px] md:w-[120px] md:h-[120px]    mr-2 shadow-lg`} src={data.info.program_mini_icon} width={150} height={150} alt={data.info.program_name} /></div>
+                                <div className='flex flex-col my-auto '>
+                                    <div className='flex'>
+                                        <div className='text-gray-700 text-base md:text-lg  flex align-item-center'> <Link href={`/`}>หน้าหลัก</Link> <ChevronRight className='w-5 h-5 md:w-8 md:h-8' /><Link href={`/program/${data.info.program_slug}`} className='flex font-bold '>{data.info.program_name}</Link> </div>
+                                    </div>
+                                    <div className='flex'>
+                                        <ShareProgram data={data} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='container mx-auto bg-white'>
+                            <OptimizedImage className={`    w-full h-full  shadow-lg`} src={data.info.tvp_img_main} width={700} height={550} alt={data.info.program_name} />
+                        </div>
+                        <div className='container mx-auto bg-white text-center my-10'>
+                            <h1 className='text-gray-700 font-bold text-3xl'>{data.info.program_name}</h1>
+                            <h2 className='text-gray-600 text-xl'>{data.info.program_desc}</h2>
+                        </div>
+                        <div className='container mx-auto bg-white'>
+                            <SlideNews data={data.news_feature.items} />
+                        </div>
+                        <div className='container mx-auto bg-white min-w-[300px] min-h-[250px]'>
+                            <AdUnit id='div-gpt-ad-1676443162388-0' adUnitPath='/33368840/TA_Desktop_Program_MidLeaderboard' size={[[728, 90], [970, 250], [970, 90], [1120, 300]]} />
+                        </div>
+                        <div className='container mx-auto bg-white text-gray-700 text-xl p-4 md:p-0 md:text-3xl'>
+                            {parse(data.info.tvp_profile_html)}
+                        </div>
+                        <div className='container mx-auto bg-white'>
+                            <ProgramsListNews data={data} />
+                        </div>
+                    </div>
+                  
+                </div>
+            </main>
+            </MobileOnly>
+
 
 
         </>
