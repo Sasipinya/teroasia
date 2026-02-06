07031fc4ff02e4ecc9bc58c72712cd5de114624a
Sasipinya Charoenratthamongkol <sasipinya.c@teroasia.com>
2026-02-06 12:17:03 +0700

    update New Home Design

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -12,16 +12,27 @@ export default function HomeLayout({
 }) {
   return (
     <>
-     <link rel="stylesheet" href="/assets/css/vendor/bootstrap.min.css" />
-        <link rel="stylesheet" href="/assets/css/vendor/fontawesome.css" />
-        <link rel="stylesheet" href="/assets/css/vendor/aos.css" />
-        <link rel="stylesheet" href="/assets/css/vendor/magnific-popup.css" />
-        <link rel="stylesheet" href="/assets/css/vendor/slick-slider.css" />
-        <link rel="stylesheet" href="/assets/css/vendor/nice-select.css" />
-        <link rel="stylesheet" href="/assets/css/vendor/odometer.css" />
-        <link rel="stylesheet" href="/assets/css/vendor/mobile.css" />
-        <link rel="stylesheet" href="/assets/css/vendor/sidebar.css" />
-        <link rel="stylesheet" href="/assets/css/main.css" />
+    {/* Preload CSS - โหลดก่อนทุกอย่าง */}
+      <link rel="preload" href="/assets/css/vendor/bootstrap.min.css" as="style" />
+      <link rel="preload" href="/assets/css/vendor/fontawesome.css" as="style" />
+      <link rel="preload" href="/assets/css/main.css" as="style" />
+      
+      {/* โหลด CSS จริง */}
+      <link rel="stylesheet" href="/assets/css/vendor/bootstrap.min.css" />
+      <link rel="stylesheet" href="/assets/css/vendor/fontawesome.css" />
+      <link rel="stylesheet" href="/assets/css/vendor/aos.css" />
+      <link rel="stylesheet" href="/assets/css/vendor/magnific-popup.css" />
+      <link rel="stylesheet" href="/assets/css/vendor/slick-slider.css" />
+      <link rel="stylesheet" href="/assets/css/vendor/nice-select.css" />
+      <link rel="stylesheet" href="/assets/css/vendor/odometer.css" />
+      <link rel="stylesheet" href="/assets/css/vendor/mobile.css" />
+      <link rel="stylesheet" href="/assets/css/vendor/sidebar.css" />
+      <link rel="stylesheet" href="/assets/css/main.css" />
+      
+      {/* Swiper & Animate */}
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
       {children}
     </>
   )
