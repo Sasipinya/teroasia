7de8bd50670ef7b4dc5e4367f118f9ac3e5fbedf
Sasipinya Charoenratthamongkol <sasipinya.c@teroasia.com>
2026-02-09 18:11:42 +0700

    update picture

--- app/components/home/Terodigital.tsx
+++ app/components/home/Terodigital.tsx
@@ -8,16 +8,16 @@ const swiperOptions = {
 	modules: [Autoplay, Pagination, Navigation],
 	slidesPerView: 3,
 	spaceBetween: 30,
-	
 
-	
+
+
 
 	// Pagination
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
 	},
-	breakpoints:{
+	breakpoints: {
 		320: {
 			slidesPerView: 1,
 			spaceBetween: 30,
@@ -57,60 +57,66 @@ export default function Terodigital() {
 		return (
 			<div className="memory1-section-area sp1">
 				<div className="container">
-					
+					<div className="row">
+						<div className="col-lg-6 m-auto">
+							<div className="blog-header text-center heading2 space-margin60">
+								<img src="https://terodigital.com/wp-content/uploads/2025/06/LogoTeroDigital_www-2.png" alt="Terodigital logo" width={300} />
+							</div>
+						</div>
+					</div>
 					<div className="row">
 						<div className="col-lg-12 memory-slider-area">
 							<Swiper {...swiperOptions} className=" owl-carousel">
-							<SwiperSlide className="memory-boxarea">
-								<div className="img1 image-anime">
-										<img src="/assets/img/logo/tinchok.png" alt="" />
-								</div>
-								<div className="content-area">
-									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
-									<div className="arrow">
-										<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+								<SwiperSlide className="memory-boxarea">
+									<div className="img1 image-anime">
+										<img src="/assets/img/elements/thediscussion-thumb_0.jpg" alt="" />
 									</div>
-									<div className="space18" />
-									<p>ออกอากาศทางช่อง one31 ทุกวันจันทร์ - ศุกร์ เวลา 15.45 - 16.30 น. และรับชมรีรันได้ทางช่อง GMM25 เวลา 18.00 - 18.45 น.</p>
-									<div className="space12" />
-									<Link href="/memories">ถกไม่เถียง</Link>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide className="memory-boxarea">
-								<div className="img1 image-anime">
-									<img src="/assets/img/logo/nt.png" alt="" />
-								</div>
-								<div className="content-area">
-									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
-									<div className="arrow">
-										<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+									<div className="content-area">
+										<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
+										<div className="arrow">
+											<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+										</div>
+										<div className="space18" />
+										<p>ออกอากาศทางช่อง one31 ทุกวันจันทร์ - ศุกร์ เวลา 15.45 - 16.30 น. และรับชมรีรันได้ทางช่อง GMM25 เวลา 18.00 - 18.45 น.</p>
+										<div className="space12" />
+										<Link href="/memories">ถกไม่เถียง</Link>
 									</div>
-									<div className="space18" />
-									<p>ออกอากาศทุกวันจันทร์ - ศุกร์ เวลา 07.50 - 08.00 น. ทางช่อง one31 รับชมรีรันได้ทางช่อง GMM25 เวลา 10.30 - 10.40 น. และช่องทางออนไลน์ TERO Digital</p>
-									<div className="space12" />
-									<Link href="/memories">เงินทองของจริง</Link>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide className="memory-boxarea">
-								<div className="img1 image-anime">
-									<img src="/assets/img/logo/kf.png" alt="" />
-								</div>
-								<div className="content-area">
-									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
-									<div className="arrow">
-										<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+								</SwiperSlide>
+								<SwiperSlide className="memory-boxarea">
+									<div className="img1 image-anime">
+										<img src="/assets/img/elements/money-guru-thumb_0.jpg" alt="" />
 									</div>
-									<div className="space18" />
-									<p>"Kids Fun คิดฝัน" คือพื้นที่แห่งความสุขและการเรียนรู้สำหรับเด็กๆ</p>
-									<div className="space12" />
-									<Link href="/memories">Kidsfun</Link>
-								</div>
-							</SwiperSlide>
-							
-						</Swiper>
+									<div className="content-area">
+										<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
+										<div className="arrow">
+											<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+										</div>
+										<div className="space18" />
+										<p>ออกอากาศทุกวันจันทร์ - ศุกร์ เวลา 07.50 - 08.00 น. ทางช่อง one31 รับชมรีรันได้ทางช่อง GMM25 เวลา 10.30 - 10.40 น. และช่องทางออนไลน์ TERO Digital</p>
+										<div className="space12" />
+										<Link href="/memories">เงินทองของจริง</Link>
+									</div>
+								</SwiperSlide>
+								<SwiperSlide className="memory-boxarea">
+									<div className="img1 image-anime">
+										<img src="/assets/img/elements/kidsfun-thumb_0.jpg" alt="" />
+									</div>
+									<div className="content-area">
+										<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
+										<div className="arrow">
+											<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+										</div>
+										<div className="space18" />
+										<p>"Kids Fun คิดฝัน" คือพื้นที่แห่งความสุขและการเรียนรู้สำหรับเด็กๆ</p>
+										<div className="space12" />
+										<Link href="/memories">Kidsfun</Link>
+									</div>
+								</SwiperSlide>
+
+							</Swiper>
 
 
-							
+
 						</div>
 					</div>
 				</div>
@@ -122,13 +128,19 @@ export default function Terodigital() {
 	return (
 		<div className="memory1-section-area sp1">
 			<div className="container">
-				
+				<div className="row">
+					<div className="col-lg-6 m-auto">
+						<div className="blog-header text-center heading2 space-margin60">
+							<img src="https://terodigital.com/wp-content/uploads/2025/06/LogoTeroDigital_www-2.png" alt="Terodigital logo" width={300} />
+						</div>
+					</div>
+				</div>
 				<div className="row">
 					<div className="col-lg-12 memory-slider-area">
 						<Swiper {...swiperOptions} className=" owl-carousel">
 							<SwiperSlide className="memory-boxarea">
 								<div className="img1 image-anime">
-										<img src="/assets/img/logo/tinchok.png" alt="" />
+									<img src="/assets/img/elements/thediscussion-thumb_0.jpg" alt="" />
 								</div>
 								<div className="content-area">
 									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
@@ -143,7 +155,7 @@ export default function Terodigital() {
 							</SwiperSlide>
 							<SwiperSlide className="memory-boxarea">
 								<div className="img1 image-anime">
-									<img src="/assets/img/logo/nt.png" alt="" />
+									<img src="/assets/img/elements/money-guru-thumb_0.jpg" alt="" />
 								</div>
 								<div className="content-area">
 									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
@@ -158,7 +170,7 @@ export default function Terodigital() {
 							</SwiperSlide>
 							<SwiperSlide className="memory-boxarea">
 								<div className="img1 image-anime">
-									<img src="/assets/img/logo/kf.png" alt="" />
+									<img src="/assets/img/elements/kidsfun-thumb_0.jpg" alt="" />
 								</div>
 								<div className="content-area">
 									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
@@ -171,10 +183,10 @@ export default function Terodigital() {
 									<Link href="/memories">Kidsfun</Link>
 								</div>
 							</SwiperSlide>
-							
+
 						</Swiper>
 
-						
+
 					</div>
 				</div>
 			</div>
