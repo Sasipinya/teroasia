e06b4fedf5ab0c1cdec82ed168eacea1951f4b38
Sasipinya Charoenratthamongkol <sasipinya.c@teroasia.com>
2026-02-10 09:46:48 +0700

    Update logo terodigital

--- app/components/home/Terodigital.tsx
+++ app/components/home/Terodigital.tsx
@@ -59,61 +59,61 @@ export default function Terodigital() {
 				<div className="container">
 					<div className="row">
 						<div className="col-lg-6 m-auto">
-							<div className="blog-header text-center heading2 space-margin60">
-								<img src="https://terodigital.com/wp-content/uploads/2025/06/LogoTeroDigital_www-2.png" alt="Terodigital logo" width={300} />
+							<div className="flex justify-center blog-header text-center heading2 space-margin60">
+								<img src="https://terodigital.com/wp-content/uploads/2025/06/LogoTeroDigital_www-2.png" alt="Terodigital logo" width={200} />
 							</div>
 						</div>
 					</div>
 					<div className="row">
 						<div className="col-lg-12 memory-slider-area">
 							<Swiper {...swiperOptions} className=" owl-carousel">
-								<SwiperSlide className="memory-boxarea">
-									<div className="img1 image-anime">
-										<img src="/assets/img/elements/thediscussion-thumb_0.jpg" alt="" />
+							<SwiperSlide className="memory-boxarea">
+								<div className="img1 image-anime">
+									<img src="/assets/img/elements/thediscussion-thumb_0.jpg" alt="" />
+								</div>
+								<div className="content-area">
+									<img src="https://terodigital.com/wp-content/uploads/2025/07/1tok.png" alt="" className="logo1 keyframe5" />
+									<div className="arrow">
+										<Link href="http://ถกไม่เถียง.com/"><i className="fa-solid fa-arrow-right" /></Link>
 									</div>
-									<div className="content-area">
-										<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
-										<div className="arrow">
-											<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
-										</div>
-										<div className="space18" />
-										<p>ออกอากาศทางช่อง one31 ทุกวันจันทร์ - ศุกร์ เวลา 15.45 - 16.30 น. และรับชมรีรันได้ทางช่อง GMM25 เวลา 18.00 - 18.45 น.</p>
-										<div className="space12" />
-										<Link href="/memories">ถกไม่เถียง</Link>
+									<div className="space18" />
+									<p>ออกอากาศทางช่อง one31 ทุกวันจันทร์ - ศุกร์ เวลา 15.45 - 16.30 น. และรับชมรีรันได้ทางช่อง GMM25 เวลา 18.00 - 18.45 น.</p>
+									<div className="space12" />
+									<Link href="http://ถกไม่เถียง.com/">ถกไม่เถียง</Link>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide className="memory-boxarea">
+								<div className="img1 image-anime">
+									<img src="/assets/img/elements/money-guru-thumb_0.jpg" alt="" />
+								</div>
+								<div className="content-area">
+									<img src="https://terodigital.com/wp-content/uploads/2025/07/3Money.png" alt="" className="logo1 keyframe5" />
+									<div className="arrow">
+										<Link href="https://www.เงินทองของจริง.com/"><i className="fa-solid fa-arrow-right" /></Link>
 									</div>
-								</SwiperSlide>
-								<SwiperSlide className="memory-boxarea">
-									<div className="img1 image-anime">
-										<img src="/assets/img/elements/money-guru-thumb_0.jpg" alt="" />
+									<div className="space18" />
+									<p>ออกอากาศทุกวันจันทร์ - ศุกร์ เวลา 07.50 - 08.00 น. ทางช่อง one31 รับชมรีรันได้ทางช่อง GMM25 เวลา 10.30 - 10.40 น. และช่องทางออนไลน์ TERO Digital</p>
+									<div className="space12" />
+									<Link href="https://www.เงินทองของจริง.com/">เงินทองของจริง</Link>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide className="memory-boxarea">
+								<div className="img1 image-anime">
+									<img src="/assets/img/elements/kidsfun-thumb_0.jpg" alt="" />
+								</div>
+								<div className="content-area">
+									<img src="https://terodigital.com/wp-content/uploads/2025/07/2kidsfun.png" alt="" className="logo1 keyframe5" />
+									<div className="arrow">
+										<Link href="https://teroasia.com/program/kidsnews"><i className="fa-solid fa-arrow-right" /></Link>
 									</div>
-									<div className="content-area">
-										<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
-										<div className="arrow">
-											<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
-										</div>
-										<div className="space18" />
-										<p>ออกอากาศทุกวันจันทร์ - ศุกร์ เวลา 07.50 - 08.00 น. ทางช่อง one31 รับชมรีรันได้ทางช่อง GMM25 เวลา 10.30 - 10.40 น. และช่องทางออนไลน์ TERO Digital</p>
-										<div className="space12" />
-										<Link href="/memories">เงินทองของจริง</Link>
-									</div>
-								</SwiperSlide>
-								<SwiperSlide className="memory-boxarea">
-									<div className="img1 image-anime">
-										<img src="/assets/img/elements/kidsfun-thumb_0.jpg" alt="" />
-									</div>
-									<div className="content-area">
-										<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
-										<div className="arrow">
-											<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
-										</div>
-										<div className="space18" />
-										<p>"Kids Fun คิดฝัน" คือพื้นที่แห่งความสุขและการเรียนรู้สำหรับเด็กๆ</p>
-										<div className="space12" />
-										<Link href="/memories">Kidsfun</Link>
-									</div>
-								</SwiperSlide>
+									<div className="space18" />
+									<p>"Kids Fun คิดฝัน" คือพื้นที่แห่งความสุขและการเรียนรู้สำหรับเด็กๆ</p>
+									<div className="space12" />
+									<Link href="https://teroasia.com/program/kidsnews">Kidsfun</Link>
+								</div>
+							</SwiperSlide>
 
-							</Swiper>
+						</Swiper>
 
 
 
@@ -130,8 +130,8 @@ export default function Terodigital() {
 			<div className="container">
 				<div className="row">
 					<div className="col-lg-6 m-auto">
-						<div className="blog-header text-center heading2 space-margin60">
-							<img src="https://terodigital.com/wp-content/uploads/2025/06/LogoTeroDigital_www-2.png" alt="Terodigital logo" width={300} />
+						<div className="flex justify-center blog-header text-center heading2 space-margin60">
+							<img src="https://terodigital.com/wp-content/uploads/2025/06/LogoTeroDigital_www-2.png" alt="Terodigital logo" width={200} />
 						</div>
 					</div>
 				</div>
@@ -143,14 +143,14 @@ export default function Terodigital() {
 									<img src="/assets/img/elements/thediscussion-thumb_0.jpg" alt="" />
 								</div>
 								<div className="content-area">
-									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
+									<img src="https://terodigital.com/wp-content/uploads/2025/07/1tok.png" alt="" className="logo1 keyframe5" />
 									<div className="arrow">
-										<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+										<Link href="http://ถกไม่เถียง.com/"><i className="fa-solid fa-arrow-right" /></Link>
 									</div>
 									<div className="space18" />
 									<p>ออกอากาศทางช่อง one31 ทุกวันจันทร์ - ศุกร์ เวลา 15.45 - 16.30 น. และรับชมรีรันได้ทางช่อง GMM25 เวลา 18.00 - 18.45 น.</p>
 									<div className="space12" />
-									<Link href="/memories">ถกไม่เถียง</Link>
+									<Link href="http://ถกไม่เถียง.com/">ถกไม่เถียง</Link>
 								</div>
 							</SwiperSlide>
 							<SwiperSlide className="memory-boxarea">
@@ -158,14 +158,14 @@ export default function Terodigital() {
 									<img src="/assets/img/elements/money-guru-thumb_0.jpg" alt="" />
 								</div>
 								<div className="content-area">
-									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
+									<img src="https://terodigital.com/wp-content/uploads/2025/07/3Money.png" alt="" className="logo1 keyframe5" />
 									<div className="arrow">
-										<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+										<Link href="https://www.เงินทองของจริง.com/"><i className="fa-solid fa-arrow-right" /></Link>
 									</div>
 									<div className="space18" />
 									<p>ออกอากาศทุกวันจันทร์ - ศุกร์ เวลา 07.50 - 08.00 น. ทางช่อง one31 รับชมรีรันได้ทางช่อง GMM25 เวลา 10.30 - 10.40 น. และช่องทางออนไลน์ TERO Digital</p>
 									<div className="space12" />
-									<Link href="/memories">เงินทองของจริง</Link>
+									<Link href="https://www.เงินทองของจริง.com/">เงินทองของจริง</Link>
 								</div>
 							</SwiperSlide>
 							<SwiperSlide className="memory-boxarea">
@@ -173,14 +173,14 @@ export default function Terodigital() {
 									<img src="/assets/img/elements/kidsfun-thumb_0.jpg" alt="" />
 								</div>
 								<div className="content-area">
-									<img src="/assets/img/icons/logo1.svg" alt="" className="logo1 keyframe5" />
+									<img src="https://terodigital.com/wp-content/uploads/2025/07/2kidsfun.png" alt="" className="logo1 keyframe5" />
 									<div className="arrow">
-										<Link href="/memories"><i className="fa-solid fa-arrow-right" /></Link>
+										<Link href="https://teroasia.com/program/kidsnews"><i className="fa-solid fa-arrow-right" /></Link>
 									</div>
 									<div className="space18" />
 									<p>"Kids Fun คิดฝัน" คือพื้นที่แห่งความสุขและการเรียนรู้สำหรับเด็กๆ</p>
 									<div className="space12" />
-									<Link href="/memories">Kidsfun</Link>
+									<Link href="https://teroasia.com/program/kidsnews">Kidsfun</Link>
 								</div>
 							</SwiperSlide>
 
