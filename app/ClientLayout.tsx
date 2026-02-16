0eedfac5984e6a37399add9d950ce45a8e291ea3
Sasipinya Charoenratthamongkol <sasipinya.c@teroasia.com>
2026-02-16 10:23:22 +0700

    update

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -85,7 +85,7 @@ export default function ClientLayout({
       <link rel="stylesheet" href="/assets/css/vendor/slick-slider.css" />
       <link rel="stylesheet" href="/assets/css/vendor/nice-select.css" />
       <link rel="stylesheet" href="/assets/css/vendor/odometer.css" />
-      <link rel="stylesheet" href="/assets/css/vendor/mobile.css" />
+      <link rel="stylesheet" href={`/assets/css/vendor/mobile.css?v=${cssVersion}`}/>
       <link rel="stylesheet" href="/assets/css/vendor/sidebar.css" />
       <link rel="stylesheet" href={`/assets/css/main.css?v=${cssVersion}`} />
 
